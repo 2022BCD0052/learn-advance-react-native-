8c36c565aa8fb2fb4539c54b1d420ba49c6cb288
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2024-12-12 11:05:28 +0530

    learn reddux

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ImagecomponentExample from './src/Components/ImagecomponentExample'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      {/* <Imagecompone jntExample/> */}
+    </View>
+  )
+}
+
+export default App
